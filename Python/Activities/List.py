2eb0286c43c67515a81004687bc87ca8166c7195
BaljeetJanjua <105587277+BaljeetJanjua@users.noreply.github.com>
2022-05-30 15:56:51 +0530

    Add files via upload

---
+++ Python/Activities/List.py
@@ -0,0 +1,12 @@
+example_list = ["apple", "banana", "cherry"]
+example_list[1] = "blackcurrant"
+print(example_list) 
+
+example_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(example_list[:2])
+
+# Loop to get all list items
+
+example_list = ["apple", "banana", "cherry"]
+for item in example_list:
+  print(item) 
